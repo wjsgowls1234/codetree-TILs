159f819695edad18761877cb8ffb04d20ea46e93
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-07-12 10:35:46 +0900

    Title: 특정 조건 세 정수 비교, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240712/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,14 @@
+arr=input().split()
+a=int(arr[0])
+b=int(arr[1])
+c=int(arr[2])
+
+if a == min(arr):
+    print(1, end=" ")
+else:
+    print(0, end=" ")
+
+if a == b == c:
+    print(1)
+else:
+    print(0)
