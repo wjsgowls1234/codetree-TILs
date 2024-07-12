747e86246a3ef6a645c0575e0e9696f2577213d3
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-07-12 10:21:14 +0900

    Title: 비교 연산, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240712/비교 연산/comparison-operator.py
@@ -0,0 +1,34 @@
+arr=input().split()
+
+a=int(arr[0])
+b=int(arr[1])
+
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if b >= a:
+    print(1)
+else:
+    print(0)
+
+if b > a:
+    print(1)
+else:
+    print(0)
+
+if a == b:
+    print(1)
+else:
+    print(0)
+
+if a != b:
+    print(1)
+else:
+    print(0)
