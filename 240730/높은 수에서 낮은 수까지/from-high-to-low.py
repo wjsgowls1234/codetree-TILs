be15321fe8adb501b4a1212222b4367b9c1c2b21
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-07-30 13:55:02 +0900

    Title: 높은 수에서 낮은 수까지, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240730/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,10 @@
+arr=input().split()
+a=int(arr[0])
+b=int(arr[1])
+
+if a >= b:
+    for i in range(a,b-1,-1):
+        print(i, end=" ")
+else:
+    for i in range(b,a-1,-1):
+        print(i, end=" ")
