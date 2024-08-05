4f0ab78c5dc0dcc92a8e87b733a2e7acea2a2fac
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-05 11:35:26 +0900

    Title: ab 사이에 있는 c, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240805/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,14 @@
+arr=input().split()
+a=int(arr[0])
+b=int(arr[1])
+c=int(arr[2])
+satisfied = False
+
+for i in range(a,b+1):
+    if i % c == 0:
+        satisfied = True
+
+if satisfied == True:
+    print("YES")
+else:
+    print("NO")
