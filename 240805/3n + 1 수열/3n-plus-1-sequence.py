8eabe2ef4196c280bcb384e139f2aceca2360867
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-05 10:20:27 +0900

    Title: 3n + 1 수열, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/3n + 1 수열/3n-plus-1-sequence.py
+++ 240805/3n + 1 수열/3n-plus-1-sequence.py
@@ -2,13 +2,13 @@ n=int(input())
 cnt = 0
 
 while True:
-    if n % 2 == 0:
-        n /= 2
-        cnt += 1
-    else:
-        n = n * 3 + 1
-        cnt += 1
     if n == 1:
         break
+
+    if n % 2 == 0:
+        n //= 2
+    else:
+        n = n * 3 + 1
+    cnt += 1
 
 print(cnt)
