fef197076881765990cc5cee4813c7ad23d40cbb
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-07 13:34:38 +0900

    Title: 별표 출력하기 5, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240807/별표 출력하기 5/print-star-5.py
+++ 240807/별표 출력하기 5/print-star-5.py
@@ -2,7 +2,7 @@ n=int(input())
 
 for i in range(n):
     for j in range(n-i):
-        for k in range(n-i-1):
+        for k in range(n-i):
             print("*", end="")
-        print("*", end=" ")
+        print(" ", end="")
     print()
