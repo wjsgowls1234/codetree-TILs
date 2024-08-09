7db8193370939609077222af1fa99efb77999e54
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-09 14:32:47 +0900

    Title: 정상적인 구구단, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/정상적인 구구단/normal-multiple.py
+++ 240809/정상적인 구구단/normal-multiple.py
@@ -2,7 +2,7 @@ n=int(input())
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        print(f"{i} * {j} = {i*j}", end=" ")
+        print(f"{i} * {j} = {i*j}", end="")
 
         if j < 3:
             print(",", end=" ")
