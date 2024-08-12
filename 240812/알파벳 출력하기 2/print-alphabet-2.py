0ba353f7e3b6e5be63f0b6056c6339eedc9ffee4
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-12 13:17:23 +0900

    Title: 알파벳 출력하기 2, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/알파벳 출력하기 2/print-alphabet-2.py
+++ 240812/알파벳 출력하기 2/print-alphabet-2.py
@@ -2,11 +2,11 @@ n=int(input())
 cnt="A"
 
 for i in range(n):
-    if ord(cnt) > ord("Z"):
-        cnt = "A"
     for j in range(i):
         print(" ", end=" ")
     for j in range(n-i):
         print(cnt, end=" ")
         cnt = chr(ord(cnt) + 1)
+        if ord(cnt) > ord("Z"):
+            cnt="A"
     print()
