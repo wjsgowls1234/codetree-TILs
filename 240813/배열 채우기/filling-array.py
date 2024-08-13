f4a0c61dd4ee5bd36e8b90106f29980255be6ee3
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-13 10:51:09 +0900

    Title: 배열 채우기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240813/배열 채우기/filling-array.py
@@ -0,0 +1,19 @@
+arr=list(map(int,input().split()))
+
+
+#for elem in reversed_arr:
+    #print(elem, end=" ")
+
+
+for i in range(10):
+    arr[i]
+    
+    reversed_arr = arr[::-1]
+    #for j in range():
+        #print(arr[j], end=" ")
+
+    if arr[i] == 0:
+        #cnt += 1
+        break
+
+    print(reversed_arr[i], end=" ")
