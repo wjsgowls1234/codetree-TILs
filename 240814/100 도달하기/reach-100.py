2aebd665d884268ed66fa72ed2986ebc8cfe95f9
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-14 10:54:52 +0900

    Title: 100 도달하기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/100 도달하기/reach-100.py
+++ 240814/100 도달하기/reach-100.py
@@ -1,10 +1,10 @@
 n=int(input())
 
-arr=[0,0,0]
-arr[1], arr[2]=1, n
+arr=[0,0]
+arr[0], arr[1]=1, n
 
-for i in range(1, 11): 
+for elem in arr: 
     arr.append(arr[-1] + arr[-2])
-    print(arr[i], end=" ")
-    if arr[i] > 100:
+    print(elem, end=" ")
+    if elem > 100:
         break
