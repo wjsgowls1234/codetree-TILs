fad55fbe5ee7fa0a4003f2eb2951f124fd500cde
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-19 10:55:40 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240819/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+prev_max_idx = n
+
+while True:
+    max_idx = 0
+
+    for i in range(1, prev_max_idx):
+        if arr[i] > arr[max_idx]:
+            max_idx = i
+
+    print(max_idx + 1, end = " ")
+
+    if max_idx == 0:
+        break
+
+    prev_max_idx = max_idx
