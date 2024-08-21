35f62953f687cef4bccae87c5f4105a88fb93346
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-21 10:46:52 +0900

    Title: 특정 문자의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240821/특정 문자의 유무/specific-character-presence.py
+++ 240821/특정 문자의 유무/specific-character-presence.py
@@ -1,16 +1,11 @@
 string = input()
 
-length = len(string)
-exists1 = False
-exists2 = False
-
-for i in range(length - 1):
-    if string[i] == 'e' and string[i + 1] == 'e':
-        exists1 = True
-    if string[i] == 'a' and string[i + 1] == 'b':
-        exists2 = True
-
-if exists1 == True or exists2 == True:
+if 'ee' in string:
     print("Yes", end=" ")
-if exists1 == False or exists2 == False:
+else:
+    print("No", end=" ")
+
+if 'ab' in string:
+    prin("Yes")
+else:
     print("No")
