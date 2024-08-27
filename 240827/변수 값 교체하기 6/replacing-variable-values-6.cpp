211c8bff685847fd31f63b5592106f48a2ccb1e3
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-27 11:45:23 +0900

    Title: 변수 값 교체하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240827/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 2, b = 5;
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << endl;
+    cout << b;
+    return 0;
+}
