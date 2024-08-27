07fd51e8e96978cfab7e96da27e93914130b0db1
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-27 13:04:07 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240827/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int y, m ,d;
+    char s;
+
+    cin >> y >> s >> m >> s >> d;
+
+    cout << m << "-" << d << "-" << y;
+    return 0;
+}
