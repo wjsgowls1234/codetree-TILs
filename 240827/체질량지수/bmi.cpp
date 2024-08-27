cd3881dfc269f0593b1456740306a9ab674f9bcb
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-27 17:21:40 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240827/체질량지수/bmi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int h, w;
+    double b;
+    cin >> h >> w;
+
+    b = (10000 * w) / (h * h);
+    
+    cout << b << endl;
+
+    if (b >= 25) {
+        cout << "Obesity";
+    }
+
+    return 0;
+}
