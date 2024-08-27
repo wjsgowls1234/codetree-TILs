9a73c0733805a8297a219f27c560c9939782df8e
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-27 12:26:21 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240827/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    double n;
+    cin >> n;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << n * 30.48;
+    return 0;
+}
