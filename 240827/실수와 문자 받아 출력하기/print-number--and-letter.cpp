c3552cca7967074f08b2304698cb3d08719da5da
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-27 12:46:30 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240827/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char c;
+    double a, b;
+    cin >> c >> a >> b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << c << endl;
+    cout << a << endl;
+    cout << b;
+    return 0;
+}
