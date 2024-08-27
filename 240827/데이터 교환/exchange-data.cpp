e192eae03afe88fd6406e46b81a917b02bc17d7b
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-27 11:55:54 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240827/데이터 교환/exchange-data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 5, b = 6, c = 7;
+    int temp;
+
+    temp = a;
+    a = c;
+    c = b;
+    b = temp;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c;
+
+    return 0;
+}
