194835ab9cedc148439bdfed5a48121dabea446d
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-28 11:34:17 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240828/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 240828/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -3,16 +3,40 @@ using namespace std;
 
 int main() {
     
-    int a, b, c;
+    int a, b, c, minimum;
     cin >> a >> b >> c;
 
-    if ((a > b && b > c) && (a == c)) {
+    if (a > b && b > c) {
+        minimum = c;
+    }
+    else if (a > c && c > b) {
+        minimum = b;
+    }
+    else if (b > a && a > c) {
+        minimum = c;
+    }
+    else if (b > c && c > a) {
+        minimum = a;
+    }
+    else if (c > b && b > a) {
+        minimum = a;
+    }
+    else if (c > a && a > b) {
+        minimum = b;
+    }
+
+    if (a == minimum) {
         cout << "1 ";
     }
     else {
         cout << "0 ";
     }
 
-    cout << (a == b == c);
+    if (a == b == c) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
     return 0;
 }
