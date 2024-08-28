93efe5ed031a6ee954a24818feda9242d725e098
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-28 16:51:17 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240828/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int b, a;
+    cin >> b >> a;
+
+    for (int i = b; i >= a; i -= 2)
+        cout << i << " ";
+    return 0;
+}
