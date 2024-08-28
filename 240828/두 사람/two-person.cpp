0a56ca70c7aeae07d8d4bd6ec7fd8f07343a6c8a
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-28 13:14:07 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240828/두 사람/two-person.cpp
+++ 240828/두 사람/two-person.cpp
@@ -9,7 +9,7 @@ int main() {
     cin >> a_age >> a_sex;
     cin >> b_age >> b_sex;
 
-    if ((a_age >= 19 || b_age >= 19) && (a_sex == 'M' || b_sex == 'M'))
+    if ((a_age >= 19 && a_sex == 'M') or (b_age >= 19 && b_sex == 'M'))
         cout << 1;
     else
         cout << 0;
