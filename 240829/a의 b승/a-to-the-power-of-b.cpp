87480b4c9aa040144756ef7c87ea1def6fbac66f
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-29 14:31:18 +0900

    Title: a의 b승, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240829/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    cin >> a >> b;
+
+    int prod = 1;
+
+    for (int i = 1; i <= b; i++) {
+        prod *= a;
+    }
+
+    cout << prod;
+    return 0;
+}
