1b3ba7132e847fe8368990f7f496ef965b0eea7f
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-29 10:00:08 +0900

    Title: 3의 배수 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240829/3의 배수 출력하기/print-multiple-of-number-three.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n, i = 3;
+    cin >> n;
+
+    while (i <= n) {
+        cout << i << " ";
+        i += 3;
+    }
+    return 0;
+}
