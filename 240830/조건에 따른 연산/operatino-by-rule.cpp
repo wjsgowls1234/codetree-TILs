60ba44a58de36f254eda5782211acad585c90c5b
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-30 13:59:50 +0900

    Title: 조건에 따른 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240830/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    int cnt = 0;
+
+    while (n < 1000) {
+
+        if (n % 2 == 0) {
+            n = n * 3 + 1;
+        }
+        else {
+            n = n * 2 + 2;
+        }
+
+        cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
