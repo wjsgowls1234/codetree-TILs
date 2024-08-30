8d0091327df26ed5122ed2c77625f47279e74c04
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-30 14:16:15 +0900

    Title: 공약수의 유무, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240830/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    cin >> a >> b;
+    bool satisfied = false;
+
+    for (int i = a; i <= b; i++) {
+        if (1920 % i == 0 && 2880 % i == 0) {
+            satisfied = true;
+        }
+    }
+
+    if (satisfied == true) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
+}
