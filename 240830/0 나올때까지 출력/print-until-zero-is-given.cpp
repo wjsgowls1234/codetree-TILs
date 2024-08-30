8736587194bb8eb35cb32ab7b096d6f593f2adee
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-08-30 10:50:25 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240830/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    for (;;) {
+        cin >> n;
+
+        if (n == 0) {
+            break;
+        }
+        
+        cout << n << endl;
+    }
+    return 0;
+}
