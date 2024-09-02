67b9b8214ae75949cb6c2bca58f9784069333d6d
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-09-02 17:33:09 +0900

    Title: 건너뛰고 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240902/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl << endl;
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = n - i; j >= 1; j--) {
+            cout << "*";
+        }
+        cout << endl << endl;
+    }
+    return 0;
+}
