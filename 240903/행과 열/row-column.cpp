9bd1b11f10a8b43af03c9d449022cfb97861480c
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-09-03 21:35:13 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240903/행과 열/row-column.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= a; i++) {
+        for (int j = 1; j <= b; j++) {
+            cout << i * j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
