9ee5cf7e033a6b95f44bf3fe04371bb03335eb3d
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-09-04 13:27:51 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240904/구구단 만들기 2/print-multiplication-table-2.cpp
+++ 240904/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -9,7 +9,7 @@ int main() {
     for (int i = 2; i <= 8; i += 2) {
         for (int j = b; j >= a; j--) {
             cout << j << " * " << i << " = " << i * j;
-            if (j != 2) {
+            if (j != a) {
                 cout << " / ";
             }
         }
