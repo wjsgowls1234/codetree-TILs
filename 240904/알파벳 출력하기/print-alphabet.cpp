ecfc033e33210966b5ed68b901f07395c2f74741
wjsgowls1234 <166822957+wjsgowls1234@users.noreply.github.com>
2024-09-04 15:40:13 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240904/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    char cnt = 'A';
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << cnt;
+            cnt++;
+
+            if (cnt > 'Z') {
+                cnt = 'A';
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
